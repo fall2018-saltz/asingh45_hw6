2ae4f33ca6ea11c59170406288417b696936fe33
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:29:38 -0400

    Saving read_census.R

--- read_census.R
+++ read_census.R
@@ -1,1 +1,3 @@
 
+census <- read.csv(url('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv')
+                     ,stringsAsFactors = FALSE)
