7b20c5e01dc5c35859e4ca7ffe9fc74edd8c9d50
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:52:50 -0400

    Saving read_census.R

--- read_census.R
+++ read_census.R
@@ -1,6 +1,7 @@
 
 #Reading the census data from the url using read.csv
 clean_df <- function(){
+  census <- read.csv(census)
   census <- census[-c(1,53),c(5:8)]
   colnames(census) <- c("stateName","population","popOver18","percentOver18")
   return(census)
