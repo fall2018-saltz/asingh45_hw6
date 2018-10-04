ee1f57b3a0097ea1eea4db80399fa2445e654e91
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:37:30 -0400

    Saving read_census.R

--- read_census.R
+++ read_census.R
@@ -1,4 +1,9 @@
 
 #Reading the census data from the url using read.csv
-census <- read.csv(url('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv')
-                     ,stringsAsFactors = FALSE)
+clean_df <- function(){
+  census <- read.csv(url('https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv')
+           ,stringsAsFactors = FALSE)
+  census <- census[-c(1,53),c(5:8)]
+  colnames(census) <- c("stateName","population","popOver18","percentOver18")
+  return(census)
+}
