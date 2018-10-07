b015d5b6d8eb7c09d16d0ba60b63211c3b5b1338
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 19:37:39 -0400

    Saving visualizations.R

--- visualizations.R
+++ visualizations.R
@@ -1,6 +1,6 @@
 
 #head(df)
 library(ggplot2)
-png('population_hist.png')
+#png('population_hist.png')
 ggplot(aes(population),data = df) + geom_histogram()
-dev.off()
+ggsave('population_hist.png')
