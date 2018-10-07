d787a82ecf00f56e28adef20be4f3f8c9a9d5bb5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 19:45:19 -0400

    Saving visualizations.R

--- visualizations.R
+++ visualizations.R
@@ -1,4 +1,7 @@
 
+colnames(df)
 library(ggplot2)
 population_hist = ggplot(aes(population),data = df) + geom_histogram()
 ggsave('population_hist.png',plot = population_hist)
+
+#murder_rate = ggplot(aes)
