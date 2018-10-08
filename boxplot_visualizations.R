79f9d00e3e97186c6bb233152735b362bf1e0ac5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 20:12:59 -0400

    Saving boxplot_visualizations.R

--- boxplot_visualizations.R
+++ boxplot_visualizations.R
@@ -3,6 +3,6 @@ library(ggplot2)
 population_box = ggplot(aes(y=population,x=stateName),data = df) + geom_boxplot()
 ggsave('population_box.png',plot = population_box)
 
-#murder_rate_box = ggplot(aes(y=Murder),data = df) + geom_boxplot()
-#ggsave('murder_rate_box.png',plot = murder_rate_box)
+murder_rate_box = ggplot(aes(y=Murder),data = df) + geom_boxplot()
+ggsave('murder_rate_box.png',plot = murder_rate_box)
 
