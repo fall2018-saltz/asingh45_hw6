d8aa5ba776332f3c06b60ae3e6b513d87b439591
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 11:53:56 -0400

    Saving murder_rate_analysis.R

--- murder_rate_analysis.R
+++ murder_rate_analysis.R
@@ -3,6 +3,7 @@
 murder_df <- data.frame(cbind(df$stateName,df$Murder*df$population))
 colnames(murder_df) <- c('stateName','NoOfMurders')
 head(murder_df)
+
 library(ggplot2)
 ggplot(aes(x=stateName,y=NoOfMurders),data=murder_df) + geom_col() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
