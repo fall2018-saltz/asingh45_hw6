5bf657f944606b419ceffa57f10c02d2445be120
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 18:15:56 -0400

    Saving murder_rate_analysis.R

--- murder_rate_analysis.R
+++ murder_rate_analysis.R
@@ -13,9 +13,11 @@ state_order <- df[order(df$NoOfMurders),2]
 ggplot(aes(x=factor(stateName,levels = state_order),y=NoOfMurders),data=df) + geom_col() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   ggtitle('Total Murders')
+ggsave('barplot2.png')
 
 ggplot(aes(x=factor(stateName,levels = state_order),y=NoOfMurders),data=df) + 
   geom_col(color = df$percentOver18) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x="States",y="Number of Murders") +
   ggtitle('Total Murders')
+ggsave('barplot3.png')
