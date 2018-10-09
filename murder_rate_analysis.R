83cef3acadd33ed4bca56b03424f6c23fcb6b2a0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 11:17:54 -0400

    Saving murder_rate_analysis.R

--- murder_rate_analysis.R
+++ murder_rate_analysis.R
@@ -1,5 +1,4 @@
 
 #head(df)
 murder_df <- data.frame(cbind(df$stateName,df$Murder*df$population))
-murder_df['stateName'] <- df$stateName
-murder_df['NoOfMurders'] <- df$Murder*df$population
+colnames(murder_df) <- c('stateName','NoOfMurders')
