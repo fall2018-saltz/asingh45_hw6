edf3b450aa11ab73d6b383e373132c08be8a29bb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:18:55 -0400

    Saving boxplot_visualizations.R

--- boxplot_visualizations.R
+++ boxplot_visualizations.R
@@ -8,3 +8,4 @@ murder_rate_box = ggplot(aes(y=Murder),data=df)+geom_boxplot()
 ggsave('murder_rate_box.png',plot = murder_rate_box)
 
 #As we see from the plots the boxplot shows a similar trend as the histogram
+#showing the right skewness of the population and close to normal spread
