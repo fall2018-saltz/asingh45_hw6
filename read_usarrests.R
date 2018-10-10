5c0be299d8388267ac1e3a42fbc33a448dc74e3a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 13:38:59 -0400

    Saving read_usarrests.R

--- read_usarrests.R
+++ read_usarrests.R
@@ -1,4 +1,5 @@
 
 #Getting the USArrests dataframe and storing it in a dataframe object for our use
 arrests <- USArrests
+#As the name of the states are saved as the row names in this dataframe we 
 arrests['stateName'] <- row.names(arrests)
