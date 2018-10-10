e1b18bb442c1ecc0e4c0cbb06d36217aeaf46c7a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 14:21:01 -0400

    Saving boxplot_visualizations.R

--- boxplot_visualizations.R
+++ boxplot_visualizations.R
@@ -10,5 +10,6 @@ ggsave('murder_rate_box.png',plot = murder_rate_box)
 #As we see from the plots the boxplot shows a similar trend as the histogram
 #showing the right skewness of the population and close to normal spread
 #of murder rate
-#However boxplots show this information more concisely and also highlight 
+#However boxplots show this information more concisely and also highlights 
 #factors like the 25th, 75th and the median and shows outliers more 
+#intuitively and hence I find it more expressive.
